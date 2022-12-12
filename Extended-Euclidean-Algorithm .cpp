085f133eb796f3367e528676daff64a8e8304010
Cipher-20 <76740873+Cipher-20@users.noreply.github.com>
2022-12-12 21:58:27 +0200

    Update Extended-Euclidean-Algorithm .cpp

--- Extended-Euclidean-Algorithm .cpp
+++ Extended-Euclidean-Algorithm .cpp
@@ -1,65 +1,57 @@
-#include<bits/stdc++.h>
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define dpp(arr,val) memset(arr,val,sizeof(arr))
-#define ll long long
-#define ull unsigned long long
-#define dd double
-#define ld long double
-#define PQ priority_queue
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define S second
-#define F first
-#define MP make_pair
-#define endl "\n"
-#define PI 3.14159265
-using namespace std;
-const int sz = 1e6 + 10;
-const int OO = 0x3f3f3f3f;
+// the logic for extended eucldean algorithm is not full main. I use some code from one Arabic guy :)
 
-int extendedEuclid_recursive(int a, int b, int& x, int& y) {
+#include <iostream>
+
+using namespace std;
+
+int ext_gcd(int a, int b, int& x, int& y) {
     if (b == 0) {
         x = 1;
         y = 0;
+
         return a;
     }
+
     int x1, y1;
-    int d = extendedEuclid_recursive(b, a % b, x1, y1);
+
+    int d = ext_gcd(b, a % b, x1, y1);
+
     x = y1;
     y = x1 - y1 * (a / b);
-    return d;
-}
 
-int extendedEuclid_iterative(int a, int b, int& x_prev, int& y_prev)
-{
-    x_prev = 1, y_prev = 0;
-    int x = 0, y = 1;
-    while (b) {
-        int q = a / b;
-        tie(x, x_prev) = make_tuple(x_prev - q * x, x);
-        tie(y, y_prev) = make_tuple(y_prev - q * y, y);
-        tie(a, b) = make_tuple(b, a % b);
+    return d;
+ }
+
+
+int main() {
+
+    int a, b, c, x, y;
+
+    cin >> a >> b >> c;
+
+    if (1 <= a && a <= 100 && 1 <= b && b <= 100 && 1 <= c && c <= 100) {
+
+        int g = ext_gcd(a, b, x, y);
+
+
+        x = x * (c / (g));
+        y = y * (c / (g));
+
+      //  int result = a*x + b*y;
+
+        if (c % g == 0){
+
+            cout << x << endl;
+            cout << y << endl;
+
+        }
+        else {
+
+            cout << "No solution!";
+        }
 
     }
-
-    return a;
-}
-
-int main()
-{
-    //myf.open("file.txt");
-    //freopen("task.in", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int a, b;
-    cin >> a >> b;
-    int x, y;
-    cout << extendedEuclid_recursive(a, b, x, y) << " ";
-    cout << x << " " << y << endl;
-
-    cout << extendedEuclid_iterative(a, b, x, y) << " ";
-    cout << x << " " << y << endl;
-
-    return 0;
-
+    else {
+        cout << "Invalid input data!";
+    }
 }
