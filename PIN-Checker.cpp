81fd6d02ec5c459d7421772e38b1f9a193abfb09
Cipher-20 <76740873+Cipher-20@users.noreply.github.com>
2022-12-19 23:56:07 +0200

    edit program

--- PIN-Checker.cpp
+++ PIN-Checker.cpp
@@ -8,27 +8,39 @@ using namespace std;
 int main() {
 
 // personal identification number
-	long long int pin;
+	string pin = "";
 
 	cin >> pin;
 
-	string size = to_string(pin);
+	int length = pin.length();
+
+	char* array = new char[length];
+
 	
-	int length = size.length();
 
 	if (length == 10) {
 
-		long long int number = pin;
 
-		int year = number / 100000000;
+		for (int i = 0; i < length; i++){
 
-		int month = number / 1000000;
-		month = month % 100;
+			array[i] = pin[i];
+		}
+
+		//for ( int i = 0; i < length; i++){
 
 
-		int day = number / 10000;
-		day = day % 100;
+		//	cout << array[i];
+		//}
 
+		string months = "";
+		months = array[2];
+		months += array[3];
+
+		int month = stoi(months);
+
+
+
+		
 		if (21 <= month && month <= 32){
 
 			month = month - 20;
@@ -39,26 +51,36 @@ int main() {
 			month = month - 40;
 		}
 
-		int x1 = year / 10;
-		int x2 = year % 10;
+		int x1s = array[0] ;
+		int x1 = x1s;
 
-		int x3 = month / 10;
-		int x4 = month % 10;
+		int x2 =  array[1];
 
-		int x5 = day / 10;
-		int x6 = day % 10;
+		int x3;
+		int x4;
+		if (month < 9){
 
-		int x7 = number / 1000;
-		x7 = x7 % 10;
+			 x3 = 0;
+			 x4 = month;
+		}
+		else {
 
-		int x8 = number / 100;
-		x8 = x8 % 10;
+		x3 =  month / 10;
 
-		int x9 = number / 10;
-		x9 = x9 % 10;
+		x4 = month % 10;
+		}
+
+		int x5 = array[4];
+		int x6 = array[5];
+
+		int x7 = array[6];
+
+		int x8 = array[7];
+
+		int x9 = array[8];
 
 		int result;
-
+		//9021011124
 		//x10 = (2 * x1 + 4 * x2 + 8 * x3 + 5 * x4 + 10 * x5 + 9 * x6 + 7 * x7 + 3 * x8 + 6 * x9) % 11
 		result = (2 * x1 + 4 * x2 + 8 * x3 + 5 * x4 + 10 * x5 + 9 * x6 + 7 * x7 + 3 * x8 + 6 * x9) % 11;
 
@@ -67,10 +89,11 @@ int main() {
 		}
 
 		cout << result;
+	
+
 	}else{
 
 		cout << "Invalid input data!";
 	}
-
-
 }
+
