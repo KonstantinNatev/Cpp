232121055a368e53d8bc76fbadf1e6b7e23ef425
Cipher-20 <76740873+Cipher-20@users.noreply.github.com>
2022-12-20 15:56:02 +0200

    Update PIN-Checker.cpp
    edit task

--- PIN-Checker.cpp
+++ PIN-Checker.cpp
@@ -51,17 +51,16 @@ int main() {
 			month = month - 40;
 		}
 
-		int x1s = array[0] ;
-		int x1 = x1s;
+		int x1 = array[0] - '0';
 
-		int x2 =  array[1];
+		int x2 =  array[1] - '0';
 
 		int x3;
 		int x4;
 		if (month < 9){
 
 			 x3 = 0;
-			 x4 = month;
+			 x4 = month ;
 		}
 		else {
 
@@ -70,25 +69,33 @@ int main() {
 		x4 = month % 10;
 		}
 
-		int x5 = array[4];
-		int x6 = array[5];
+		int x5 = array[4] - '0';
+		int x6 = array[5] - '0';
 
-		int x7 = array[6];
+		int x7 = array[6] - '0';
 
-		int x8 = array[7];
+		int x8 = array[7] - '0';
 
-		int x9 = array[8];
+		int x9 = array[8] - '0';
+
 
 		int result;
-		//9021011124
+		//9001011238
+		// 0241011120
 		//x10 = (2 * x1 + 4 * x2 + 8 * x3 + 5 * x4 + 10 * x5 + 9 * x6 + 7 * x7 + 3 * x8 + 6 * x9) % 11
-		result = (2 * x1 + 4 * x2 + 8 * x3 + 5 * x4 + 10 * x5 + 9 * x6 + 7 * x7 + 3 * x8 + 6 * x9) % 11;
+		result = ((2 * x1) + (4 * x2) + (8 * x3) + (5 * x4) + (10 * x5) + (9 * x6) + (7 * x7) + (3 * x8) + (6 * x9));
 
-		if (result == 10){
-			result = 0;
+		if (result % 11 == 0) {
+			cout << 1 << endl;
+		}
+		else if (result % 11 <= 10)
+		{
+			cout << 0 << endl;
+
+		}else {
+			cout << 0 << endl;
 		}
 
-		cout << result;
 	
 
 	}else{
@@ -96,4 +103,3 @@ int main() {
 		cout << "Invalid input data!";
 	}
 }
-
