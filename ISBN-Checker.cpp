9fa487254c209fed9fe7cb902fbadfa9780d9982
Cipher-20 <76740873+Cipher-20@users.noreply.github.com>
2022-12-20 15:58:32 +0200

    edit task

--- ISBN-Checker.cpp
+++ ISBN-Checker.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
+#include <string>
+#include <cmath>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
 
-using namespace std;
+int main() {
 
-int main() { // Task 2 Corrrect ISBN code 0-7167-0344-0
-	char isbn[14], * isbn_ptr;
-	int* isbn_numbers = new int[10];
-	cout << "Please enter an ISBN code: ";
-	cin >> isbn;
+    long long int ISBN;
 
-	isbn_ptr = isbn;
-	int i = 0;
-	while (*isbn_ptr != '\0') {
-		if (*isbn_ptr != '-' && *isbn_ptr != 'X') {
-			isbn_numbers[i++] = (int)(*isbn_ptr) - 48;
-		}
-		if (*isbn_ptr == 'X') {
-			isbn_numbers[i++] = 10;
-		}
-		isbn_ptr++;
-	}
-	int sum = 0;
-	for (int i = 0, j = 1; i < 10; i++, j++) {
-		cout << j << " * " << isbn_numbers[i] << " + ";
-		sum += j * isbn_numbers[i];
-	}
-	cout << "\nThe sum is: " << sum << endl;
-	cout << "The ISBN number is" << ((sum % 11) ? " not " : " ") << "correct" << endl;
+    cout << "Enter IBSN: ";
+    cin >> ISBN;
+    
+    /*
+    if (((1 * (ISBN % 10) + 2 * (ISBN / 10 % 10) + 3 * (ISBN / 100 % 10)
+        + 4 * (ISBN / 1000 % 10) + 5 * (ISBN / 10000 % 10)
+        + 6 * (ISBN / 100000 % 10) + 7 * (ISBN / 1000000 % 10)
+        + 8 * (ISBN / 10000000 % 10) + 9 * (ISBN / 100000000 % 10)
+        + 10 * (ISBN / 1000000000 % 10)) % 11) == 0) {
+        cout << "Korekten e!" << endl;
+    }
+    else {
+        cout << "NE E KOREKTEN" << endl;
+    }
+    */
+
+    int result = 0;
+
+    for (int i = 1; i <= 10; i++) {
+        result = result + (i * (ISBN / (int)(pow(10, i-1)) % 10));
+    }
+
+    if (result % 11 == 0) {
+        cout << "Correct ISBN!" << endl;
+    }
+    else {
+        cout << "Not correct ISBN!" << endl;
+    }
+
+
+    return 0;
 }
